df6f671708cde2aab24bf59fd837ddab8f33780b
Taha <taha.othman@outlook.com>
2023-03-08 15:40:23 -0500

    write all requaired functions

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+
+function writeCards(names, event){
+    const arrayOfMessages = [];
+    for(let i=0; i<names.length; i++){
+        let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+        arrayOfMessages.push(message); 
+    }
+    return arrayOfMessages;
+}
+
+function countDown(startNumber){
+    let i = startNumber;
+    while(i>=0){
+        console.log(i);
+        i--;
+    }
+}
